cebf199776a2a37afaaff7b93eb450f638853ff2
Leonardo Oliveira Balsalobre <oliveirabalsa2@gmail.com>
2025-02-12 02:02:01 -0300

    feat: setup main with docs

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { HtmlExceptionFilter } from './common/filters/html-exepction.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({ origin: 'http://localhost:8080' });
+  app.useGlobalFilters(new HtmlExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('URL Shortener API')
+    .setDescription('API for shortening URLs')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
