6f25fa503fcda5502517fe26f4f51402724de179
Leonardo Oliveira Balsalobre <oliveirabalsa2@gmail.com>
2025-02-12 11:24:13 -0300

    feat: add module redirect

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -11,7 +11,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', '..', 'client'),
+      rootPath: join(__dirname, '..', 'client'),
       exclude: ['/api*'],
     }),
     ThrottlerModule.forRoot([{ ttl: 60, limit: 15 }]),
