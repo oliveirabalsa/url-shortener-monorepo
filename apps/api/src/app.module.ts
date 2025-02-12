59954e4a63fbad59c44dfbe2639f915ea279e888
Leonardo Oliveira Balsalobre <oliveirabalsa2@gmail.com>
2025-02-12 13:31:57 -0300

    refactor: add new build version

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,15 +5,9 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UrlModule } from './url/url.module';
 import { AccountModule } from './account/account.module';
 import { AuthModule } from './auth/auth.module';
-import { join } from 'path';
-import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', '..', 'web', 'dist'),
-      exclude: ['api/*'],
-    }),
     ThrottlerModule.forRoot([{ ttl: 60, limit: 15 }]),
     PrismaModule,
     UrlModule,
