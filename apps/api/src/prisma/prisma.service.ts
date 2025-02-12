1f92c4fade248f02698b5943f8551e421ad222c4
Leonardo Oliveira Balsalobre <oliveirabalsa2@gmail.com>
2025-02-12 02:01:33 -0300

    feat: add prisma module

---
+++ apps/api/src/prisma/prisma.service.ts
@@ -0,0 +1,26 @@
+import {
+  Injectable,
+  OnModuleInit,
+  INestApplication,
+  OnModuleDestroy,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+  enableShutdownHooks(app: INestApplication) {
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    process.on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
