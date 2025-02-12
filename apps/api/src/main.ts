3d4a319efdfbf8cf2582dfa7f4f393138c5cd429
Leonardo Oliveira Balsalobre <oliveirabalsa2@gmail.com>
2025-02-12 10:20:32 -0300

    feat: add routes for vercel.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(3000);
 }
